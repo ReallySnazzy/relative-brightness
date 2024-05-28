f6e1570097e00740388d5e6f7de09c6c16b0d0da
Jonathan Cooper <drowsysaturn@gmail.com>
2024-05-28 16:35:48 -0400

    Use numeric comparison

--- relbrightness.sh
+++ relbrightness.sh
@@ -6,10 +6,10 @@ elif [[ "$1" = "dec" ]]; then
     cur=$(( cur - 10 ));
 fi
 
-if [[ $cur > 255 ]]; then
+if (( $cur > 255 )); then
     cur=255;
-elif [[ $cur < 0 ]]; then
-    cur=0
+elif (( $cur < 1 )); then
+    cur=1
 fi
 
 brightnessctl set $cur
